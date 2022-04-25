6c00e878d31ddb6e40f49e8a1dd438e757d2c258
Twigster <treshagaye@gmail.com>
2022-04-25 19:47:31 -0400

    updated login & signup components

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';  // not using
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  // replaces the addUser() functionality imported from the API file with ADD_USER mutation
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +34,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({ variables: { ...userFormData }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
